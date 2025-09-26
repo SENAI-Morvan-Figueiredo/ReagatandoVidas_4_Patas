9db75f23808ac1f9640b7690721180a34aac7ec3
Bruna <brunabarbozasofia@gmail.com>
2025-09-26 16:49:47 -0300

    fazendo login

--- ong/administrador/admin.py
+++ ong/administrador/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# administrador/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Adminstrador  # Importe seu modelo
+
+# Crie uma classe para personalizar a exibição no admin (opcional, mas recomendado)
+class AdminstradorAdmin(admin.ModelAdmin):
+    # Mostra estes campos na lista de administradores
+    list_display = ('email', 'id') 
+    
+    # Adiciona um campo de busca para facilitar encontrar usuários
+    search_fields = ('email',)
+
+# Registre seu modelo com a classe de personalização
+admin.site.register(Adminstrador, AdminstradorAdmin)
